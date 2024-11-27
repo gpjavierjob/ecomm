9140f59c97d72d699f2bd7343ed9dfe426c73218
Javier Gómez Pérez <gpjavierjob@gmail.com>
2024-11-27 11:36:11 -0300

    Adicionada la configuración de Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { initializeApp } from "firebase/app";
+
 import App from './App.jsx'
 
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB51HzqkJxOK1bUQUg9N4vdo5uAxR4OEs0",
+  authDomain: "ecom-ce536.firebaseapp.com",
+  projectId: "ecom-ce536",
+  storageBucket: "ecom-ce536.firebasestorage.app",
+  messagingSenderId: "303794712647",
+  appId: "1:303794712647:web:bd189a0fd907c019728162"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createRoot(document.getElementById("root")).render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
